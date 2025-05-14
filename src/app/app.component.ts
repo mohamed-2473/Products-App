f239060ae05e58e791f3dbfaedaf25630457d8ce
MHMD <115650241+281mohamed@users.noreply.github.com>
2025-05-14 17:28:18 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from "./components/products/products.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductsComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/produc-card/produc-card.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-produc-card',
+  imports: [CommonModule],
+  templateUrl: './produc-card.component.html',
+  styleUrl: './produc-card.component.css'
+})
+export class ProducCardComponent {
+  @Input() productItem: any;
+}
